3fc085a3063520871f9376777020e76ef95e3cc1
TheHuntsman4 <anikethvij464@gmail.com>
2023-05-28 02:57:17 +0530

    feat- combines frontend files

---
+++ cross-on/src/pages/login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('https://django.biscuitbobby.me/login/', {
+        email,
+        password,
+      });
+
+      // Handle the response from the Django login endpoint
+      if (response.status === 200) {
+        // Authentication successful
+        // Redirect or perform any desired action
+        console.log('Login successful!');
+      } else {
+        // Authentication failed
+        // Display an error message or perform any desired action
+        console.error('Login failed!');
+        console.log(email);
+        console.log(password);
+      }
+    } catch (error) {
+      // Handle any errors that occur during the request
+      console.error('An error occurred during login:', error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleLogin}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
