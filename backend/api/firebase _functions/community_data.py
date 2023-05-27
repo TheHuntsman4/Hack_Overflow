6dcf33dbfb73bdcd919380d8255b7e0a818de00d
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-05-27 20:11:22 +0530

    firebase read

--- backend/api/firebase _functions/community_data.py
+++ backend/api/firebase _functions/community_data.py
@@ -43,26 +43,17 @@ class CommunityDetails:
 
 class CommunityChannel:
     def __init__(self, uid, channel):
-        doc_roles_ref = db.document(f'community/{uid}/channels/channels/{channel}/{channel}')
+        doc_roles_ref = db.document(f'community/{uid}/channels/{channel}/chat/roles')
         roles_privelages = doc_roles_ref.get()
 
         # chat collection
-        chats_ref = db.collection('community').document('com1').collection('channels').document(
-            'channels').collection('channel-1').document('channel-1').collection('chat')
+        chats_ref = db.document(f'community/{uid}/channels/{channel}/chat/messages')
 
         # Retrieve all chats
         chats = chats_ref.get()
+
         # all the chat ids are stored here
-        self.LesText = []
-
-        # all the chats are stored here:
-        self.ChatData = dict()
-
-        # add all the chat ids to LesChats and chat logs to the ChatData
-        for chat in chats:
-            self.LesText.append(chat.id)
-            self.ChatData[chat.id] = chat.to_dict()
-
+        self.messages = (chats.to_dict())
 
         # Check for document
         if roles_privelages.exists:
@@ -71,11 +62,31 @@ class CommunityChannel:
             print("Document not found")  # change this into an api response
 
 
-class ListCommunityChannels:
-    def __init__(self, uid):
-        doc_roles_ref = db.collection(f'community/{uid}/channels/channels/')
-        channels = doc_roles_ref.get()
-        print(channels.to_dict())
+def ListCommunityChannels(uid):
+    document_ref = db.collection('community').document(uid).collection('channels')
+
+    # Retrieve all the channel documents
+    docs = document_ref.get()
+
+    # the channel ids are stored here
+    channels = []
+
+    for doc in docs:
+        channel_id = doc.id
+        channels.append(channel_id)
+
+    return channels
+
+
+def ListUsers(uid):
+    collection_ref = db.collection('community').document(uid).collection('members')
+    docs = collection_ref.get()
+    for doc in docs:
+        Data = doc.to_dict()
+    return Data
+
+
+
 def CommunityJson(uid):
     com = CommunityDetails(uid)
     return {
@@ -85,10 +96,28 @@ def CommunityJson(uid):
         'description': com.description
     }
 
-com = CommunityJson('com1')
 
-'''print(com)
-channels = CommunityChannel('com1', 'channel-1')
-print(channels.roles)
-for i in channels.LesText:
-    print(channels.ChatData[i])'''
+# returns the community details as a json
+print('---community details---')
+com = CommunityJson('com1')
+print(com)
+# x-------------------------------x
+
+# return the channel user roles and chat
+channel = CommunityChannel('com1', 'channel-1')
+print('---user roles---')
+print(channel.roles)
+print('---chat data---')
+print(channel.messages)
+# x-------------------------------x
+
+# list all the channels in community
+print('---list channels---')
+channels = ListCommunityChannels('com1')
+print(channels)
+# x-------------------------------x
+# list all the admin in community
+print('---list admin---')
+userlist = ListUsers('com1')
+print(userlist)
+# x-------------------------------x
